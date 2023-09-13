907259987681ceda87e3d0cfa70bcb7e16d28099
ara-yjx <yejiaxi.ac@gmail.com>
2023-09-14 05:20:25 +0900

    intl enUS

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import '@arco-design/web-react/dist/css/arco.css';
-import { Layout } from '@arco-design/web-react';
+import { ConfigProvider, Layout } from '@arco-design/web-react';
+import enUS from '@arco-design/web-react/es/locale/en-US';
 import { MainForm } from './component/mainForm';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Layout style={{ maxWidth: 1080, margin: '0 auto' }}>
-        <Layout.Header>
-          <h1>AMP Generator</h1>
-        </Layout.Header>
-        <Layout.Content>
-          <MainForm />
-        </Layout.Content>
-      </Layout>
-    </div>
+    <ConfigProvider locale={enUS}>
+      <div className="App">
+        <Layout style={{ maxWidth: 1080, margin: '0 auto' }}>
+          <Layout.Header>
+            <h1>AMP Generator</h1>
+          </Layout.Header>
+          <Layout.Content>
+            <MainForm />
+          </Layout.Content>
+        </Layout>
+      </div>
+    </ConfigProvider>
   );
 }
 
