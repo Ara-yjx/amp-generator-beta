d9cbc05d411fcf70105523ddacd47936762d2af2
ara-yjx <yejiaxi.ac@gmail.com>
2023-12-18 20:35:18 +0900

    warn before window unload

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@arco-design/web-react/dist/css/arco.css';
 import { ConfigProvider, Layout } from '@arco-design/web-react';
 import enUS from '@arco-design/web-react/es/locale/en-US';
 import { MainForm } from './component/mainForm';
 import './App.css';
 
+function warnBeforeUnload() {
+  return 'Leaving the page will reset all settings. Have you saved your settings?';
+};
+
 function App() {
+  useEffect(() => {
+    window.onbeforeunload = warnBeforeUnload;
+  });
+
   return (
     <ConfigProvider locale={enUS}>
       <div className="App">
