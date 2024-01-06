5620cb17c5fb98f05a4e1940443d6c256c37153f
ara-yjx <yejiaxi.ac@gmail.com>
2024-01-06 22:45:09 +0900

    [336c] keyboard Enter key

--- src/component/acceptedKeys.tsx
+++ src/component/acceptedKeys.tsx
@@ -7,10 +7,10 @@ function allowedKeys(key: string) {
   if (key.length === 1) {
     if (key >= '0' && key <= '9') return key;
     if (key >= 'a' && key <= 'z') return key;
-    if (key === ' ') return 'Space';
     if (key >= 'A' && key <= 'Z') return key.toLowerCase();
+    if (key === ' ') return 'Space';
   }
-  if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(key)) {
+  if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter'].includes(key)) {
     return key;
   }
   return false;
