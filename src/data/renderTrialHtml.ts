73fed930c22f991ed282ecbb1a65b3110651f5b1
ara-yjx <yejiaxi.ac@gmail.com>
2024-08-26 23:42:29 +0900

    refactor renderTrialHtml rename param name, split renderTrialHtmlForLayout

--- src/data/renderTrialHtml.ts
+++ src/data/renderTrialHtml.ts
@@ -20,12 +20,12 @@ export function getElementPoolMappingOfLayout<T = null>(layout: number[], fillWi
   return layout.map(row => Array(row).fill(fillWith));
 }
 
-function renderItem(params: AmpTrialHtml, index: number) {
+function renderItem(props: AmpTrialHtml, index: number) {
   return trim(`
     <!-- Item -->
-    <div class="spt-trial-content spt-trial-content-${index + 1}" style="margin-left: ${params.concurrentHorizontalGap ?? 0}px; width: ${params.width}px; height: ${params.height}px; position: relative;">
+    <div class="spt-trial-content spt-trial-content-${index + 1}" style="margin-left: ${props.concurrentHorizontalGap ?? 0}px; width: ${props.width}px; height: ${props.height}px; position: relative;">
       <img class="spt-trial-image" style="position: absolute; width: 100%; height: 100%; object-fit: contain;"/>
-      <div class="spt-trial-text" style="position: absolute; width: 100%; height: 100%; white-space: ${params.textWrap ? 'pre-line' : 'pre'}; color: ${params.textColor ?? 'auto'}; font-weight: ${params.textIsBold ? 'bold' : 'normal'}; font-size: ${params.textFontSize}px; line-height: 1.5em; display: flex; justify-content: center; align-items: center;"></div>
+      <div class="spt-trial-text" style="position: absolute; width: 100%; height: 100%; white-space: ${props.textWrap ? 'pre-line' : 'pre'}; color: ${props.textColor ?? 'auto'}; font-weight: ${props.textIsBold ? 'bold' : 'normal'}; font-size: ${props.textFontSize}px; line-height: 1.5em; display: flex; justify-content: center; align-items: center;"></div>
     </div>
   `);
 }
@@ -34,32 +34,35 @@ function renderItem(params: AmpTrialHtml, index: number) {
  * @param numOfCols Number of cols in this row
  * @param numOfPrevCols The total count of items in the previous rows, which is also the item index of the first col in this row 
  */
-function renderRow(params: AmpTrialHtml, numOfCols: number, numOfPrevCols: number) {
+function renderRow(props: AmpTrialHtml, numOfCols: number, numOfPrevCols: number) {
   return trim(`
   <!-- Row -->
-  <div style="display: flex; justify-content: center; align-items: center; margin-top: ${params.concurrentVerticalGap ?? 0}px; margin-left: ${-(params.concurrentHorizontalGap ?? 0)}px;">
-${range(numOfCols).map(colIndex => renderItem(params, numOfPrevCols + colIndex)).join('\n')}
+  <div style="display: flex; justify-content: center; align-items: center; margin-top: ${props.concurrentVerticalGap ?? 0}px; margin-left: ${-(props.concurrentHorizontalGap ?? 0)}px;">
+${range(numOfCols).map(colIndex => renderItem(props, numOfPrevCols + colIndex)).join('\n')}
   </div>
   `);
 }
 
-export function renderTrialHtml(params: AmpTrialHtml, concurrentDisplays: AmpTimeline['concurrentDisplays']) {
-  const layout = getUniversalLayout(concurrentDisplays);
-  const containerHeight = (params.height + (params.concurrentVerticalGap ?? 0)) * layout.length;
+export function renderTrialHtml(props: AmpTrialHtml, concurrentDisplays: AmpTimeline['concurrentDisplays']) {
+  return renderTrialHtmlForLayout(props, getUniversalLayout(concurrentDisplays));
+}
+
+export function renderTrialHtmlForLayout(props: AmpTrialHtml, layout: number[]) {
+  const containerHeight = (props.height + (props.concurrentVerticalGap ?? 0)) * layout.length;
   return trim(`
 <!-- Container. Fixed-height so that the instruction won't move. Flex prevents margin collapse with external. -->
-<div style="display: flex; flex-direction: column; height: ${containerHeight}px; box-sizing: content-box; padding-top: ${params.marginTop ?? 0}px; margin-top: ${-(params.concurrentVerticalGap ?? 0)}px;">
+<div style="display: flex; flex-direction: column; height: ${containerHeight}px; box-sizing: content-box; padding-top: ${props.marginTop ?? 0}px; margin-top: ${-(props.concurrentVerticalGap ?? 0)}px;">
 ${(() => {
   let rows = [], numOfPrevCols = 0;
   for (const numOfCols of layout) {
-    rows.push(renderRow(params, numOfCols, numOfPrevCols));
+    rows.push(renderRow(props, numOfCols, numOfPrevCols));
     numOfPrevCols += numOfCols;
   }
   return rows.join('\n');
 })()}
 </div>
 <!-- Instruction -->
-<div style="display: flex; justify-content: space-around; margin-top: 6em; white-space: pre-wrap; color: ${params.darkMode ? 'white' : 'black'}; text-align: center">${params.instruction}</div>
+<div style="display: flex; justify-content: space-around; margin-top: 6em; white-space: pre-wrap; color: ${props.darkMode ? 'white' : 'black'}; text-align: center">${props.instruction}</div>
 `);
 }
 
