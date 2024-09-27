cf4587351c18c034d68640caf50751c0995301c6
ara-yjx <yejiaxi.ac@gmail.com>
2024-09-27 17:41:27 +0900

    fix AT backward compatibility

--- src/data/backwardCompatibility.ts
+++ src/data/backwardCompatibility.ts
@@ -1,9 +1,10 @@
-import type { AmpParams } from './ampTypes';
+import type { AmpParams, AT } from './ampTypes';
 import { emptyAmpParams } from './emptyAmpParams';
 
 export function transformOldValues(values: AmpParams) {
   console.log('transformOldValues');
-  console.log(JSON.stringify(values))
+  console.log(JSON.stringify(values));
+
   values.trialType ??= 'simple';
   if (typeof values.trialHtml !== 'string') {
     if (values.trialHtml.text !== undefined) {
@@ -21,5 +22,30 @@ export function transformOldValues(values: AmpParams) {
       customHtml: values.trialHtml,
     }
   }
+
+
+  if (values.advancedTimeline) {
+    for (const page of values.advancedTimeline.pages) {
+
+      // Create { enabled: false } for all undefined AT response
+      // Same as advancedTimeline: emptyPage()
+      page.response.keyboard ??= { enabled: false, keys: [], delayBefore: 0, delayAfter: 0 };
+      page.response.timeout ??= { enabled: false, duration: 1000 };
+      page.response.mouseClick ??= { enabled: false };
+
+      // Previouisly  AT.Page['layoutedDisplay'] = DisplaySrc[][] 
+      // Now it's further wrapped                = { displaySrc: DisplaySrc, ... }[][]
+      page.layoutedDisplays.forEach((row, rowIndex) => {
+        row.forEach((col: unknown, colIndex) => {
+          if ((col as AT.layoutedDisplayItem).displaySrc === undefined && typeof (col as AT.DisplaySrc)[0] === 'string') {
+            page.layoutedDisplays[rowIndex][colIndex] = { displaySrc: (col as AT.DisplaySrc) };
+          }
+        })
+      });
+    }
+  }
+
+
+  console.log('=>');
   console.log(JSON.stringify(values))
 }
